e8680e57b4425cf7980e4522adfb272eb0037997
shivamd0598 <shivamdhar532@gmail.com>
2022-03-29 00:49:05 +0530

    Charing url to ec2 instance in login service

--- src/app/login.service.ts
+++ src/app/login.service.ts
@@ -7,7 +7,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class LoginService {
-  private baseUrl="http://ec2-34-222-244-44.us-west-2.compute.amazonaws.com:8080";
+  private baseUrl="http://ec2-52-34-167-231.us-west-2.compute.amazonaws.com:8080";
   constructor(private http:HttpClient) { }
   //generate token
   public generateToken(loginData:any){
